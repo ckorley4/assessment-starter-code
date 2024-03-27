e09e686c97bec513610e2980e509963a42a2ba06
ckorley4 <ckorley4uo@yahoo.com>
2024-03-27 13:42:23 -0400

    assessment completed

--- server/app.py
+++ server/app.py
@@ -5,3 +5,25 @@ from flask_restful import Resource
 
 import ipdb
 
+class Projects(Resource):
+    def get(self):
+        projects = Project.query.all()
+        project_list = [project.to_dict() for project in projects]
+        return make_response(project_list,200)
+
+class ProjectsByID(Resource):
+    def delete(self,id):
+        project = Project.query.get(id)
+        if project:
+            db.session.delete(project)
+            db.session.commit()
+            return make_response({},204)
+        else:
+            return make_response({"error:Project not found"},404)
+        
+api.add_resource(Projects,"/projects")
+api.add_resource(ProjectsByID,"/projects/<int:id>")
+
+if __name__ == "__main__":
+    app.run(port=5555, debug=True)
+
